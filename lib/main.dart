cd750e1eb8302e0188dc313a6b873f89e872fa5f
helmer <helmer>
2020-05-24 15:52:54 +0100

    create button navigation, top bar and FloactionButton

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'models/global.dart';
 
 void main() => runApp(MyApp());
 
@@ -31,13 +32,82 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
 
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
+    return MaterialApp(
+          color: Colors.yellow,
+          home: SafeArea(
+              child: DefaultTabController(
+                length: 3,
+                child: new Scaffold(
+                  body: Stack(
+                    children: <Widget>[
+                      TabBarView(
+                        children: [
+                          new Container(
+                            color: darkGreyColor,
+                          ),
+                          new Container(color: Colors.orange,),
+                          new Container(
+                            color: Colors.lightGreen,
+                          ),
+                        ],
+                      ),
+                      Container(
+                        padding: EdgeInsets.only(left: 50),
+                        height: 160,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.only(
+                            bottomLeft: Radius.circular(50),
+                            bottomRight: Radius.circular(50),
+                          ),
+                          color: Colors.white,
+                        ),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: <Widget>[
+                            Text("Notas", style: intrayTitleStyle,),
+                            Container()
+                          ],
+                        ),
+                      ),
+                      Container(
+                        height: 65,
+                        width: 80,
+                        margin: EdgeInsets.only(top: 128, left: MediaQuery.of(context).size.width*0.5 - 40),
+                        child: FloatingActionButton(
+                          child: Icon(Icons.add, size: 65,),
+                          backgroundColor: redColor,
+                          onPressed: () {},
+                        ),
+                      ),
+                    ],
+                  ),
+                  appBar: AppBar(
+                    elevation: 0,
+                    title: new TabBar(
+                      tabs: [
+                        Tab(
+                          icon: new Icon(Icons.home),
+                        ),
+                        Tab(
+                          icon: new Icon(Icons.rss_feed),
+                        ),
+                        Tab(
+                          icon: new Icon(Icons.perm_identity),
+                        ),
+                      ],
+                      labelColor: darkGreyColor,
+                      unselectedLabelColor: Colors.blue,
+                      indicatorSize: TabBarIndicatorSize.label,
+                      indicatorPadding: EdgeInsets.all(5.0),
+                      indicatorColor: Colors.transparent,
+                    ),
+                    backgroundColor: Colors.white,
+                  ),
+                  backgroundColor: Colors.white,
 
-      ),
-    );
+                ),
+              ),
+          ),
+        );
   }
 }
